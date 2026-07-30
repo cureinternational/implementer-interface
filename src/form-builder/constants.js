58a51fe4692066eb502dc8917788e5e04c97b980
SasikiranJ <sasikiran.jakkula@thoughtworks.com>
2026-07-30 18:17:26 +0530

    remove redundant constants

--- src/form-builder/constants.js
+++ src/form-builder/constants.js
@@ -29,8 +29,6 @@ export const formBuilderConstants = {
   jsonToPdfConvertionUrl: '/openmrs/ws/rest/v1/bahmniie/form/jsonToPdf',
   pdfDownloadUrl: '/openmrs/ws/rest/v1/bahmniie/form/download/',
   hyperlinkUrlProperty: 'hyperlinkUrl',
-  hyperlinkLabelProperty: 'hyperlinkLabel',
-  hyperlinkAllowedDomainsGP: 'bahmni.forms.hyperlink.allowedDomains',
   allowedDomainsGPUrl: '/openmrs/ws/rest/v1/bahmnicore/sql/globalproperty?property=bahmni.forms.hyperlink.allowedDomains',
 
 };
